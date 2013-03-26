f7518da141e803f6985df1f8f7c79df59cbd12e1
Max Goedjen <max.goedjen@gmail.com>
2013-03-25 19:43:18 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -6,13 +6,12 @@ from distutils.core import setup
 doclines = __doc__.split("\n")
 
 setup(name="flask_simple_openid",
-      version="1.0.0",
+      version="1.0.1",
       maintainer="Max Goedjen",
       maintainer_email="max.goedjen@gmail.com",
       url = "http://github.com/maxgoedjen/flask_simple_openid",
       platforms = ["any"],
       description = doclines[0],
-      readme = 'README.md',
       install_requires = ['flask_openid'],
       long_description = "\n".join(doclines[2:])
       )
