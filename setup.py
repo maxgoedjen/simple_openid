ebd06d1fdc6f9f8f562eb2e8478b0a3d607d85bf
Max Goedjen <max.goedjen@gmail.com>
2013-03-25 20:02:59 -0700

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ from distutils.core import setup
 doclines = __doc__.split("\n")
 
 setup(name="simple_openid",
-      version="1.0.1",
+      version="1.0.2",
       maintainer="Max Goedjen",
       maintainer_email="max.goedjen@gmail.com",
       url = "http://github.com/maxgoedjen/simple_openid",
       platforms = ["any"],
+      py_modules=['simple_openid'],
       description = doclines[0],
       install_requires = ['flask_openid'],
       long_description = "\n".join(doclines[2:])
